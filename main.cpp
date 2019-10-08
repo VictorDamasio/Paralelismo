d945cb0ab4a5ef80b9b89da7a58d58a7c03cdabf
trebehazuos <35035191+trebehazuos@users.noreply.github.com>
2019-10-08 20:37:25 -0300

    openmp implementado, busca de string

--- main.cpp
+++ main.cpp
@@ -1,38 +1,56 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <stdio.h>
+#include <omp.h>
 static unsigned int num_linhas = 0;
 
-struct Linha{
-    unsigned int num;
-    std::string texto;
-};
-
 void encontrarNumLinhas(std::ifstream &file);
-Linha *extrairArquivo(std::ifstream &file);
+std::string  *extrairArquivo(std::ifstream &file);
 
-int main(){
+
+int main()
+{ 
+
     std::ifstream file("exemplo.txt");
     encontrarNumLinhas(file);
-    Linha* ptr = extrairArquivo(file);
+    std::string* ptr = extrairArquivo(file);
     unsigned int x = 0;
-    while (x < num_linhas){ //IMPRIMINDO O TREIM
-        std::cout<<"Linha: " <<(ptr + x)->num <<" Texto: " <<(ptr + x)->texto <<std::endl;
+    std::string palavra="sou";
+    int palavra_size=palavra.size();
+    
+     while (x < num_linhas){ //IMPRIMINDO O TREIM
+        std::cout<<*(ptr + x) <<std::endl;
         x++;
     }
+    std::string aux="";
+    int quantidade=0,i=0,aux_size=0;
+    #pragma omp parallel for reduction(+:quantidade) private(aux,aux_size)
+    for ( i = 0; i < num_linhas; i++)
+    {
+            aux=*(ptr+i);
+            std::cout<<aux <<std::endl;
+            aux_size=aux.length();
+            for(int j=0;j<aux_size+1-palavra_size;j++){
+                if(aux.substr(j,palavra_size)==palavra)
+                    quantidade++;
+            std::cout<<quantidade <<std::endl;
+            } 
+    }
+
+std::cout<<"quantidade de palavras "<<palavra<<": "<<quantidade<<std::endl;
 
     delete[] ptr;
+    return 0;
 }
 
-Linha* extrairArquivo(std::ifstream &file){ //ARMAZENA CADA LINHA DO ARQUIVO NUM VETOR DE LINHAS
-    Linha* vet = new Linha[num_linhas];
+std::string* extrairArquivo(std::ifstream &file){ //ARMAZENA CADA LINHA DO ARQUIVO NUM VETOR DE LINHAS
+    std::string* vet = new std::string[num_linhas];
     std::string str;
     unsigned int linhaAtual = 0;
     while (linhaAtual < num_linhas){
         std::getline(file, str);
-        (vet + linhaAtual)->num = linhaAtual;
-        (vet + linhaAtual)->texto = str;
+        *(vet + linhaAtual) = str;
         linhaAtual++;
     }
     return vet;
